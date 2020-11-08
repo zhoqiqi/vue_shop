969f3b8654a40b0872e3adee947a3475b3cdf271
周其埼 <1466508194@qq.com>
2020-11-08 16:23:24 +0800

    用户功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,12 @@ const routes = [
   },
   {
     path: '/home',
-    component: () => import('../components/Home.vue')
+    component: () => import('../components/Home.vue'),
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: () => import('../components/Welcome.vue') },
+      { path: '/users', component: () => import('../components/user/Users.vue') }
+    ]
   }
 ]
 
